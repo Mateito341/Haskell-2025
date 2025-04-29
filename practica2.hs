733f1473acf98092a868b1a5e9fba3e1e4bccc00
Mateito341 <mategiovannetti@gmail.com>
2025-04-29 19:30:11 -0300

    ejercicio 10

--- practica2.hs
+++ practica2.hs
@@ -219,3 +219,50 @@ fromOrdList' xs = makeBlack root
           (leftTree, x:xs1) = build leftSize xs
           (rightTree, xs2) = build rightSize xs1
       in (T R leftTree x rightTree, xs2)
+
+--EJERCICIO 9
+-- balancea si el problema está en el subárbol izquierdo
+lbalance :: Color -> RBT a -> a -> RBT a -> RBT a
+lbalance B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
+lbalance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
+lbalance c l a r = T c l a r
+
+-- balancea si el problema está en el subárbol derecho
+rbalance :: Color -> RBT a -> a -> RBT a -> RBT a
+rbalance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
+rbalance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
+rbalance c l a r = T c l a r
+
+
+insert' :: Ord a => a -> RBT a -> RBT a
+insert' x t = makeBlack (ins x t)
+  where
+    ins x E = T R E x E
+    ins x (T c l y r)
+      | x < y     = lbalance c (ins x l) y r
+      | x > y     = rbalance c l y (ins x r)
+      | otherwise = T c l y r
+
+-- EJERCICIO 10
+type Rank = Int
+data Heap a = E | N Rank a (Heap a) (Heap a)
+
+merge :: Ord a => Heap a -> Heap a -> Heap a
+merge h1 E = h1
+merge E h2 = h2
+merge h1@(N x a1 b1) h2@(N y a2 b2) =
+  if x <= y
+    then makeH x a1 (merge b1 h2)
+    else makeH y a2 (merge h1 b2)
+
+
+rank :: Heap a → Rank
+rank E = 0
+rank (N r _ _ _) = r
+
+makeH :: a -> Heap a -> Heap a -> Heap a
+makeH x a b = if rank a > rank b then N (rank b + 1) x a b
+else N (rank a + 1) x b a
+
+ fromList :: [a] -> Heap a
+ fromList [] = E
